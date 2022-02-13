742570d542ab19d11dd10406f0b6306f48e5a530
Szymon Dziwak <skdziwak@gmail.com>
2022-02-13 12:18:18 +0100

    Refactor

--- src/main/java/com/skdziwak/factoriolang/AssemblyHumanizer.java
+++ src/main/java/com/skdziwak/factoriolang/AssemblyHumanizer.java
@@ -2,6 +2,7 @@ package com.skdziwak.factoriolang;
 
 import com.skdziwak.factoriolang.compilation.CompilationState;
 import com.skdziwak.factoriolang.compilation.Instruction;
+import com.skdziwak.factoriolang.constants.InstructionType;
 import com.skdziwak.factoriolang.constants.MathOperator;
 
 import java.util.Arrays;
@@ -25,18 +26,18 @@ public final class AssemblyHumanizer {
     }
 
     public static String humanizeState(Instruction instruction, int index) {
-        return switch (instruction.getSignalA()) {
-            case 1 -> "Copy register " + instruction.getSignalB() + " to register " + instruction.getSignalC();
-            case 2 -> "Math operation: (register 1) <- (register 1) " + humanizeOperation(instruction.getSignalB()) + " (register 2)";
-            case 3 -> "Push register " + instruction.getSignalB() + " to stack";
-            case 4 -> "Pop stack to register " + instruction.getSignalB();
-            case 5 -> "Copy register " + instruction.getSignalB() + " to RAM(" + instruction.getSignalC() + ")";
-            case 6 -> "Copy RAM(" + instruction.getSignalB() + ") to register " + instruction.getSignalC();
-            case 7 -> "Jump to " + (index + instruction.getSignalB() + 1);
-            case 8 -> "Jump to " + (index + instruction.getSignalB() + 1) + " if register 1 is equal to 0";
-            case 9 -> "Jump with dynamic offset from register 8";
-            case 10 -> "Jump with dynamic offset from register 8 if register 1 is equal to 0";
-            case 13 -> "Assign value " + instruction.getSignalC() + " to register " + instruction.getSignalB();
+        return switch (InstructionType.bySignal(instruction.getSignalA())) {
+            case COPY_REG_TO_REG -> "Copy register " + instruction.getSignalB() + " to register " + instruction.getSignalC();
+            case MATH -> "Math operation: (register 1) <- (register 1) " + humanizeOperation(instruction.getSignalB()) + " (register 2)";
+            case PUSH_REG_TO_STACK -> "Push register " + instruction.getSignalB() + " to stack";
+            case POP_STACK_TO_REG -> "Pop stack to register " + instruction.getSignalB();
+            case COPY_REG_TO_RAM -> "Copy register " + instruction.getSignalB() + " to RAM(" + instruction.getSignalC() + ")";
+            case COPY_RAM_TO_REG -> "Copy RAM(" + instruction.getSignalB() + ") to register " + instruction.getSignalC();
+            case JUMP_CONSTANT_OFFSET -> "Jump to " + (index + instruction.getSignalB() + 1);
+            case CONDITIONAL_JUMP_CONSTANT_OFFSET -> "Jump to " + (index + instruction.getSignalB() + 1) + " if register 1 is equal to 0";
+            case JUMP_DYNAMIC_OFFSET -> "Jump with dynamic offset from register 8";
+            case CONDITIONAL_JUMP_DYNAMIC_OFFSET -> "Jump with dynamic offset from register 8 if register 1 is equal to 0";
+            case SET_REGISTER -> "Assign value " + instruction.getSignalC() + " to register " + instruction.getSignalB();
             default -> "[UNKNOWN OPERATION]";
         };
     }
