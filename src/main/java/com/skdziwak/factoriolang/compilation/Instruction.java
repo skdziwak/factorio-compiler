9f6be03cb0ce5b3303ca0798b3496b56b081d342
Szymon Dziwak <skdziwak@gmail.com>
2022-02-13 10:20:24 +0100

    Promises in instructions

--- src/main/java/com/skdziwak/factoriolang/compilation/Instruction.java
+++ src/main/java/com/skdziwak/factoriolang/compilation/Instruction.java
@@ -4,48 +4,66 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Instruction {
-    private int signalA;
-    private int signalB = 0;
-    private int signalC = 0;
+    private IntegerProvider signalA;
+    private IntegerProvider signalB;
+    private IntegerProvider signalC;
 
     public Instruction(int signalA) {
-        this.signalA = signalA;
+        this.signalA = new ConstantInteger(signalA);
+        this.signalB = new ConstantInteger(0);
+        this.signalC = new ConstantInteger(0);
     }
 
     public Instruction(int signalA, int signalB) {
-        this.signalA = signalA;
-        this.signalB = signalB;
+        this.signalA = new ConstantInteger(signalA);
+        this.signalB = new ConstantInteger(signalB);
+        this.signalC = new ConstantInteger(0);
     }
 
     public Instruction(int signalA, int signalB, int signalC) {
-        this.signalA = signalA;
-        this.signalB = signalB;
-        this.signalC = signalC;
+        this.signalA = new ConstantInteger(signalA);
+        this.signalB = new ConstantInteger(signalB);
+        this.signalC = new ConstantInteger(signalC);
     }
 
     public int getSignalA() {
-        return signalA;
+        return signalA.getValue();
     }
 
     public Instruction setSignalA(int signalA) {
-        this.signalA = signalA;
+        this.signalA = new ConstantInteger(signalA);
         return this;
     }
 
     public int getSignalB() {
-        return signalB;
+        return signalB.getValue();
     }
 
     public Instruction setSignalB(int signalB) {
-        this.signalB = signalB;
+        this.signalB = new ConstantInteger(signalB);
         return this;
     }
 
     public int getSignalC() {
-        return signalC;
+        return signalC.getValue();
     }
 
     public Instruction setSignalC(int signalC) {
+        this.signalC = new ConstantInteger(signalC);
+        return this;
+    }
+
+    public Instruction setSignalA(IntegerProvider signalA) {
+        this.signalA = signalA;
+        return this;
+    }
+
+    public Instruction setSignalB(IntegerProvider signalB) {
+        this.signalB = signalB;
+        return this;
+    }
+
+    public Instruction setSignalC(IntegerProvider signalC) {
         this.signalC = signalC;
         return this;
     }
@@ -53,17 +71,31 @@ public class Instruction {
     @Override
     public String toString() {
         return "State{" +
-                "signalA=" + signalA +
-                ", signalB=" + signalB +
-                ", signalC=" + signalC +
+                "signalA=" + getSignalA() +
+                ", signalB=" + getSignalB() +
+                ", signalC=" + getSignalC() +
                 '}';
     }
 
     public Map<String, Integer> toMap() {
         Map<String, Integer> map = new HashMap<>();
-        if (signalA != 0) map.put("signal-A", getSignalA());
-        if (signalB != 0) map.put("signal-B", getSignalB());
-        if (signalC != 0) map.put("signal-C", getSignalC());
+        int a = getSignalA();
+        int b = getSignalB();
+        int c = getSignalC();
+        if (a != 0) map.put("signal-A", a);
+        if (b != 0) map.put("signal-B", b);
+        if (c != 0) map.put("signal-C", c);
         return map;
+    }
+
+    public interface IntegerProvider {
+        int getValue();
+    }
+
+    private record ConstantInteger(Integer value) implements IntegerProvider {
+        @Override
+        public int getValue() {
+            return value;
+        }
     }
 }
