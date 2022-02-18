eb3da5b7afe841f63e0bc7aa49a118dff5cec48f
Szymon Dziwak <skdziwak@gmail.com>
2022-02-18 20:29:10 +0100

    test.fish updated

--- test.fish
+++ test.fish
@@ -1,2 +1,2 @@
 #/usr/bin/env fish
-java -cp 'target/classes/:/usr/local/lib/antlr-4.9.2-complete.jar' org.antlr.v4.gui.TestRig com.skdziwak.factoriolang.antlr4.Lang prog test.fac -gui
+java -cp 'target/classes/:/usr/local/lib/antlr-4.9.2-complete.jar' org.antlr.v4.gui.TestRig com.skdziwak.factoriolang.antlr4.Lang prog test.fcl -gui
